a4876bff19a6f5fdce4c8f4ec122c16f21154303
JohnZucker <zuckerjohn@gmail.com>
2015-02-11 20:17:03 +0000

    Two source feed solution tested

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Create news website with the following requirements :
 
 https://github.com/JohnZucker/newsfeed-node    
 
-# Test server
+# Server used for tests
 
 node v0.13.0-pre    
 npm 2.3.0    
@@ -22,9 +22,8 @@ Darwin 12.5.0 Darwin Kernel Version 12.5.0
 ## Installation 
 As https://gist.github.com/isaacs/579814#file-only-git-all-the-way-sh         
 Requires node and npm    
-
-Issue    
-npm install
+ 
+    $ npm install
 
 # News feeds used
 
@@ -32,3 +31,20 @@ npm install
 * http://feeds.skynews.com/feeds/rss/home.xml
 
     * Conditions: For terms see http://www.bbc.co.uk/terms/additional_rss.shtml, http://news.sky.com/info/rss
+
+# Test runs
+
+To test locally,
+
+    $ node newsfeed-display.js &
+    Server running at http://localhost:8124/
+
+Expected console logging
+
+    Rendering articles from feedsource http://feeds.skynews.com/feeds/rss/home.xml
+    Rendering articles from feedsource http://feeds.bbci.co.uk/news/rss.xml
+    All articles written from final feed source
+
+Webpage output should resemble
+
+![newsfeed-nodejs-webpage-example.png]
--- newsfeed-display.js
+++ newsfeed-display.js
@@ -3,20 +3,26 @@ var feedparser = require('ortoo-feedparser');
 var feedsource1 = 'http://feeds.skynews.com/feeds/rss/home.xml';
 var feedsource2 = 'http://feeds.bbci.co.uk/news/rss.xml'
 
-function renderArticlesFromSource(feedsource, res, final) {
+var ARTICLES_PER_SOURCE = 10;
+
+function renderArticlesFromSource(feedsource, res, limit, final) {
     var parseStream=feedparser.parseUrl(feedsource);
+    var articles_count=0;
     console.log('Rendering articles from feedsource ' + feedsource);
     parseStream.
         on('article', function(article){
-            res.write('<li style="color:blue">' + article.title + '</li>');
-            res.write('<p>\n<small>\n<a href=' + article.link + '>' + article.link + '</a>\n</small>\n</p>');
-            res.write('<p>\n<small>' + article.summary + '</small>\n</p>');
+            if (articles_count < limit) {
+                res.write('<li style="color:blue">' + article.title + '</li>');
+                res.write('<p>\n<small>\n<a href=' + article.link + '>' + article.link + '</a>\n</small>\n</p>');
+                res.write('<p>\n<small>' + article.summary + '</small>\n</p>');
+                articles_count += 1
+            }
         });
     parseStream.
        on('end', function(){
             if (final) {
                 console.log('All articles written from final feed source')
-                res.write("</ul>\n</body>\n"); 
+                res.write("</ol>\n</body>\n"); 
                 res.end()
             }
         }); 
@@ -26,9 +32,10 @@ function renderArticlesFromSource(feedsource, res, final) {
 http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("<html>\n<title>\nLatest news headlines</title>\n<body>\n<ul>");
-    renderArticlesFromSource(feedsource1, res, false);
-    renderArticlesFromSource(feedsource2, res, true);
+    res.write("<html>\n<title>\nLatest news headlines</title>\n<body>\n<ol>");
+    renderArticlesFromSource(feedsource1, res, ARTICLES_PER_SOURCE, false);
+    renderArticlesFromSource(feedsource2, res, ARTICLES_PER_SOURCE, true);   
+    // TOOD-  Change design to put these sources into array and final member could assign true to final arg of last member
 
   }).listen(8124);
 
