c8e1e025b9a7066fece3b5915679ee94fc6ae7b9
JohnZucker <zuckerjohn@gmail.com>
2015-02-11 19:10:54 +0000

    Two named feed sources

--- newsfeed-display.js
+++ newsfeed-display.js
@@ -3,11 +3,9 @@ var feedparser = require('ortoo-feedparser');
 var feedsource1 = 'http://feeds.skynews.com/feeds/rss/home.xml';
 var feedsource2 = 'http://feeds.bbci.co.uk/news/rss.xml'
 
-http.createServer(function (req, res) {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("<html>\n<title>\nLatest news headlines</title>\n<body>\n<ul>");
-    var parseStream=feedparser.parseUrl(feedsource2);
+function renderArticlesFromSource(feedsource, res, final) {
+    var parseStream=feedparser.parseUrl(feedsource);
+    console.log('Rendering articles from feedsource ' + feedsource);
     parseStream.
         on('article', function(article){
             res.write('<li style="color:blue">' + article.title + '</li>');
@@ -16,9 +14,21 @@ http.createServer(function (req, res) {
         });
     parseStream.
        on('end', function(){
-            res.write("</ul>\n</body>\n"); 
-            res.end()
+            if (final) {
+                console.log('All articles written from final feed source')
+                res.write("</ul>\n</body>\n"); 
+                res.end()
+            }
         }); 
+}
+
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<html>\n<title>\nLatest news headlines</title>\n<body>\n<ul>");
+    renderArticlesFromSource(feedsource1, res, false);
+    renderArticlesFromSource(feedsource2, res, true);
 
   }).listen(8124);
 
